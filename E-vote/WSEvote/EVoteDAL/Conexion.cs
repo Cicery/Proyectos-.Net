df7c1880bfdf1711dc1b433d1d85681c4fad6369
Yhefferson <ycicery0@misena.edu.co>
2020-12-01 22:31:56 -0500

    Subir proyecto E-vote

---
+++ E-vote/WSEvote/EVoteDAL/Conexion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EVoteDAL
+{
+    public class Conexion
+    {
+        private static SqlConnection objConexion;
+        private static string mensaje;
+
+        /// <summary>
+        /// metodo para realizar la conexion 
+        /// </summary>
+        /// <returns>
+        /// retorna objConexion de tipo SqlConnection o null
+        /// </returns>
+
+        public static SqlConnection getConexion()
+        {
+            if (objConexion != null)
+                return objConexion;
+            objConexion = new SqlConnection();
+            objConexion.ConnectionString = @"Data Source=Cicery; Initial Catalog=DailyTasks;Integrated Security=True";
+            try
+            {
+                objConexion.Open();
+                return objConexion;
+            }
+            catch (SqlException ex)
+            {
+                mensaje = ex.Message;
+                return null;
+            }
+        }
+    }
+}
